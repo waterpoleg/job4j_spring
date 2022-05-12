b7a21cc685492f14f4c4889a2bdc5a0c350a1b0e
Oleg Korneev <waterpoleg@gmail.com>
2022-05-13 00:12:16 +0300

    1. Spring DI. Запуск [#2091]

---
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -0,0 +1,17 @@
+package ru.job4j.di;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(ConsoleInput.class);
+        context.register(StartUI.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add();
+        ui.add();
+        ui.print();
+    }
+}
